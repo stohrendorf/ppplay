c77d21769ec9e7a2f6760c5b1814af1ce94d4e1f
Syron <mr.syron@googlemail.com>
2010-11-25 07:24:33 +0100

    forgot one file to add

---
+++ src/ppg/widget.cpp
@@ -0,0 +1,245 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Syron                                         *
+ *   mr.syron@gmail.com                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "widget.h"
+#include <algorithm>
+
+namespace ppg {
+
+Point::Point() :m_x(0), m_y(0)
+{
+}
+Point::Point(int x, int y) : m_x(x), m_y(y)
+{
+}
+const Point& Point::operator+=(const Point& rhs) {
+	m_x += rhs.m_x;
+	m_y += rhs.m_y;
+	return *this;
+}
+const Point Point::operator+(const Point& rhs) const {
+	return Point(m_x+rhs.m_x, m_y+rhs.m_y);
+}
+const Point& Point::operator-=(const Point& rhs) {
+	m_x -= rhs.m_x;
+	m_y -= rhs.m_y;
+	return *this;
+}
+const Point Point::operator-(const Point& rhs) const {
+	return Point(m_x-rhs.m_x, m_y-rhs.m_y);
+}
+int Point::x() const { return m_x; }
+int Point::y() const { return m_y; }
+void Point::setX(int x) { m_x = x; }
+void Point::setY(int y) { m_y = y; }
+
+Rect::Rect(int x, int y, int width, int height) : m_topLeft(x,y), m_bottomRight(x+width-1, y+height-1)
+{
+}
+int Rect::top() const { return m_topLeft.y(); }
+void Rect::setTop(int top) { m_topLeft.setY(top); }
+int Rect::left() const { return m_topLeft.x(); }
+void Rect::setLeft(int left) { m_topLeft.setX(left); }
+int Rect::bottom() const { return m_bottomRight.y(); }
+void Rect::setBottom(int bottom) { m_bottomRight.setY(bottom); }
+int Rect::right() const { return m_bottomRight.x(); }
+void Rect::setRight(int right) { m_bottomRight.setX(right); }
+int Rect::width() const { return m_bottomRight.x() - m_topLeft.x() + 1; }
+void Rect::setWidth(int width) { m_bottomRight.setX( m_topLeft.x() + width - 1 ); }
+int Rect::height() const { return m_bottomRight.y() - m_topLeft.y() + 1; }
+void Rect::setHeight(int height) { m_bottomRight.setY( m_topLeft.y() + height - 1 ); }
+Point Rect::topLeft() const { return m_topLeft; }
+Point& Rect::topLeft() { return m_topLeft; }
+Point Rect::bottomRight() const { return m_bottomRight; }
+Point& Rect::bottomRight() { return m_bottomRight; }
+Point Rect::size() const {
+	return Point(width(), height());
+}
+
+
+Widget::Widget(Widget* parent) :
+		m_visible(false),
+		m_parent(parent),
+		m_area(0,0,0,0),
+		m_children()
+{
+	if(parent)
+		parent->m_children.push_back(this);
+}
+
+Widget::~Widget() throw() {
+	if(m_parent)
+		m_parent->m_children.remove(this);
+	std::list<Widget*> backup = m_children;
+	std::for_each(
+		backup.begin(),
+		backup.end(),
+		[](Widget*w){delete w;}
+	);
+}
+
+int Widget::setLeft(int x, bool absolute) throw() {
+	if(absolute)
+		mapToAbsolute(&x, NULL);
+	int w = m_area.width();
+	m_area.setLeft(x);
+	m_area.setWidth(w);
+	return m_area.left();
+}
+
+int Widget::setTop(int y, bool absolute) throw() {
+	if(absolute)
+		mapToAbsolute(NULL, &y);
+	int h = m_area.height();
+	m_area.setTop(y);
+	m_area.setHeight(h);
+	return m_area.top();
+}
+
+bool Widget::setPosition(int x, int y, bool absolute) throw() {
+	setLeft(x, absolute);
+	setTop(y, absolute);
+	return (m_area.left() != x) || (m_area.top() != y);
+}
+
+bool Widget::setPosition(const Point& pos, bool absolute) {
+	return setPosition(pos.x(), pos.y(), absolute);
+}
+
+int Widget::setWidth(int w) throw(Exception) {
+	PPG_TEST(w <= 0);
+	m_area.setWidth(w);
+	return m_area.width();
+}
+
+int Widget::setHeight(int h) throw(Exception) {
+	PPG_TEST(h <= 0);
+	m_area.setHeight(h);
+	return m_area.height();
+}
+
+bool Widget::setSize(int w, int h) throw(Exception) {
+	setWidth(w);
+	setHeight(h);
+	return (m_area.width() != w) || (m_area.height() != h);
+}
+bool Widget::setSize(const Point& pt) throw(Exception) {
+	return setSize(pt.x(), pt.y());
+}
+
+void Widget::draw() throw(Exception) {
+	if (!isVisible())
+		return;
+	// draw from bottom to top so that top elements are drawn over bottom ones
+	for(std::list<Widget*>::reverse_iterator revIt = m_children.rbegin(); revIt!=m_children.rend(); revIt++) {
+		Widget* w = *revIt;
+		if( !w->isVisible() )
+			continue;
+		w->draw();
+	}
+	drawThis();
+}
+
+Rect Widget::area() const {
+	return m_area;
+}
+Rect& Widget::area() {
+	return m_area;
+}
+
+void Widget::show() throw() {
+	m_visible = true;
+}
+
+void Widget::hide() throw() {
+	m_visible = false;
+}
+
+bool Widget::isVisible() const throw() {
+	return m_visible;
+}
+
+void Widget::drawChar(int x, int y, char c) throw() {
+	if ((x < 0) || (x >= m_area.width()) || (y < 0) || (y >= m_area.height()))
+		return;
+	if (m_parent) {
+		mapToParent(&x, &y);
+		m_parent->drawChar(x, y, c);
+	}
+}
+
+void Widget::drawFgColor(int x, int y, unsigned char c) throw() {
+	if ((x < 0) || (x >= m_area.width()) || (y < 0) || (y >= m_area.height()))
+		return;
+	if (m_parent) {
+		mapToParent(&x, &y);
+		m_parent->drawFgColor(x, y, c);
+	}
+}
+
+void Widget::drawBgColor(int x, int y, unsigned char c) throw() {
+	if ((x < 0) || (x >= m_area.width()) || (y < 0) || (y >= m_area.height()))
+		return;
+	if (m_parent) {
+		mapToParent(&x, &y);
+		m_parent->drawBgColor(x, y, c);
+	}
+}
+
+void Widget::mapToParent(int* x, int* y) const {
+	if (!m_parent)
+		return;
+	if (x != NULL)
+		*x += m_area.left();
+	if (y != NULL)
+		*y += m_area.top();
+}
+void Widget::mapToParent(ppg::Point* pt) const {
+	if(!m_parent || !pt)
+		return;
+	*pt += m_area.topLeft();
+}
+
+void Widget::mapToAbsolute(int* x, int* y) const {
+	if (!m_parent)
+		return;
+	mapToParent(x, y);
+	m_parent->mapToAbsolute(x, y);
+}
+void Widget::mapToAbsolute(ppg::Point* pt) const {
+	if(!m_parent || !pt)
+		return;
+	mapToParent(pt);
+	m_parent->mapToAbsolute(pt);
+}
+
+Widget *Widget::getTopParent() const {
+	if (!m_parent)
+		return NULL;
+	return m_parent->getTopParent();
+}
+
+void Widget::toTop(ppg::Widget* vp) throw() {
+	if(std::find(m_children.begin(), m_children.end(), vp)==m_children.end())
+		return;
+	m_children.remove(vp);
+	m_children.push_front(vp);
+}
+} // namespace ppg
---
+++ src/ppg/widget.h
@@ -0,0 +1,223 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Syron                                         *
+ *   mr.syron@gmail.com                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef ppgwidgetH
+#define ppgwidgetH
+
+#include "ppgexcept.h"
+#include "utils.h"
+
+#include <list>
+
+namespace ppg {
+
+/**
+ * @defgroup Ppg The PeePeeGUI Definitions
+ * @brief This module contains definitions for the PeePeeGUI User Interface
+ * @todo Check all @c operator= for self-assignments (strong guarantee?)
+ */
+
+class Point {
+	private:
+		int m_x;
+		int m_y;
+	public:
+		Point();
+		Point(int x, int y);
+		const Point& operator+=(const Point& rhs);
+		const Point operator+(const Point& rhs) const;
+		const Point& operator-=(const Point& rhs);
+		const Point operator-(const Point& rhs) const;
+		int x() const;
+		int y() const;
+		void setX(int x);
+		void setY(int y);
+};
+
+class Rect {
+	private:
+		Point m_topLeft;
+		Point m_bottomRight;
+	public:
+		Rect(int x, int y, int width, int height);
+		int top() const;
+		void setTop(int top);
+		int left() const;
+		void setLeft(int left);
+		int bottom() const;
+		void setBottom(int bottom);
+		int right() const;
+		void setRight(int right);
+		int width() const;
+		void setWidth(int width);
+		int height() const;
+		void setHeight(int height);
+		Point topLeft() const;
+		Point& topLeft();
+		Point bottomRight() const;
+		Point& bottomRight();
+		Point size() const;
+};
+
+/**
+ * @class Widget
+ * @ingroup Ppg
+ * @brief The abstract base class for all PeePeeGUI classes
+ * @details
+ * Every PeePeeGUI class is derived from this class.
+ */
+class Widget {
+		DISABLE_COPY(Widget)
+	private:
+		bool m_visible; //!< @brief @c false if this widget and it's children should not be drawn
+		Widget *m_parent; //!< @brief Pointer to the parent widget, or @c NULL if it's the top widget
+		Rect m_area;
+		std::list<Widget*> m_children; //!< @brief Children within this container
+		virtual void drawThis() throw(Exception) = 0; //!< @brief Internal drawing method, called by PppWidet::draw() @see draw()
+	public:
+		/**
+		* @brief Value that marks a non-changing color
+		 */
+		static const int ESC_NOCHANGE = 0xff;
+		/**
+		 * @brief Constructor
+		 * @param[in] name Name of this widget
+		 */
+		explicit Widget(Widget* parent);
+		/**
+		 * @brief Destructor. No operation.
+		 */
+		virtual ~Widget() throw();
+		/**
+		 * @brief Calls ppg::Widget::drawThis(), but only when ppg::Widget::aVisible is @c true
+		 */
+		virtual void draw() throw(Exception);
+		/**
+		 * @brief Make this widget visible
+		 */
+		virtual void show() throw();
+		/**
+		 * @brief Make this widget invisible
+		 */
+		virtual void hide() throw();
+		/**
+		 * @brief Draw a char relative to the widget's position
+		 * @param[in] x Left position
+		 * @param[in] y Top position
+		 * @param[in] c The char to draw
+		 */
+		virtual void drawChar(int x, int y, char c) throw();
+		/**
+		 * @brief Is this widget visible?
+		 * @return ppg::Widget::aVisible
+		 */
+		bool isVisible() const throw();
+		/**
+		 * @brief Sets the foreground color relative to the widget's position
+		 * @param[in] x Left position
+		 * @param[in] y Top position
+		 * @param[in] c Dos Color Value
+		 */
+		virtual void drawFgColor(int x, int y, unsigned char c) throw();
+		/**
+		 * @brief Sets the background color relative to the widget's position
+		 * @param[in] x Left position
+		 * @param[in] y Top position
+		 * @param[in] c Dos Color Value
+		 */
+		virtual void drawBgColor(int x, int y, unsigned char c) throw();
+		/**
+		 * @brief Sets ppg::Widget::aLeft, adjusts PpgWidget::aRight
+		 * @param[in] x Left position
+		 * @param[in] absolute Set to @c true to calculate the position relative to the top parent widget
+		 * @return New left position
+		 */
+		virtual int setLeft(int x, bool absolute = false) throw();
+		/**
+		 * @brief Sets PpgWidget::aTop, adjusts PpgWidget::aBottom
+		 * @param[in] y Top position
+		 * @param[in] absolute Set to @c true to calculate the position relative to the top parent widget
+		 * @return New top position
+		 */
+		virtual int setTop(int y, bool absolute = false) throw();
+		/**
+		 * @brief Sets PpgWidget::aLeft and PpgWidget::aTop, adjusts PpgWidget::aRight and PpgWidget::aBottom
+		 * @param[in] x Left position
+		 * @param[in] y Top position
+		 * @param[in] absolute Set to @c true to calculate the position relative to the top parent widget
+		 * @return @c false if nothing happened (no changes in PpgWidget::aLeft or PpgWidget::aTop)
+		 * @warning The returned value is (in most cases) not valid if @a absolute is @c true
+		 */
+		virtual bool setPosition(int x, int y, bool absolute = false) throw();
+		virtual bool setPosition(const Point& pos, bool absolute = false);
+		/**
+		 * @brief Adjusts PpgWidget::aRight
+		 * @param[in] w Wanted width
+		 * @return New width
+		 * @exception PpgException is thrown if @c w\<=0
+		 */
+		virtual int setWidth(int w) throw(Exception);
+		/**
+		 * @brief Adjusts PpgWidget::aBottom
+		 * @param[in] h Wanted height
+		 * @return New height
+		 * @exception PpgException is thrown if @c h\<=0
+		 */
+		virtual int setHeight(int h) throw(Exception);
+		/**
+		 * @brief Adjusts PpgWidget::aRight and PpgWidget::aBottom
+		 * @param[in] w Width
+		 * @param[in] h Height
+		 * @return @c false if nothing happened (no changes in width and height)
+		 * @exception PpgException See PpgWidget::setWidth and PpgWidget::setHeight
+		 */
+		virtual bool setSize(int w, int h) throw(Exception);
+		virtual bool setSize(const Point& pt) throw(Exception);
+		Rect area() const;
+		Rect& area();
+		/**
+		 * @brief Get the top parent
+		 * @return Pointer to the top parent
+		 */
+		virtual Widget *getTopParent() const;
+		/**
+		 * @brief Map widget's coordinates to the parent ones
+		 * @param[in,out] x Left coordinate
+		 * @param[in,out] y Top coordinate
+		 */
+		virtual void mapToParent(int *x, int *y) const;
+		virtual void mapToParent(Point* pt) const;
+		/**
+		 * @brief Map widget's coordinates to the top parent ones
+		 * @param[in,out] x Left coordinate
+		 * @param[in,out] y Top coordinate
+		 */
+		virtual void mapToAbsolute(int *x, int *y) const;
+		virtual void mapToAbsolute(Point* pt) const;
+		/**
+		 * @brief Move an element to the top
+		 * @param[in] vp Pointer to the element to move to the top
+		 */
+		virtual void toTop(ppg::Widget* vp) throw();
+};
+
+} // namespace ppg
+
+#endif // ppgwidgetH
