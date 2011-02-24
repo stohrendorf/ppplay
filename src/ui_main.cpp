4fca5b438cc712cf9cd0512de390aca994adcb85
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2011-02-24 20:17:04 +0100

    made everything GPLv3+, some include guards renamed to make them consistent

--- src/ui_main.cpp
+++ src/ui_main.cpp
@@ -1,3 +1,21 @@
+/*
+    PeePeePlayer - an old-fashioned module player
+    Copyright (C) 2010  Syron <mr.syron@googlemail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "ui_main.h"
 
 UIMain::UIMain( ppg::Widget* parent ): Widget( parent ), m_position( NULL ), m_screenSep1( NULL ), m_screenSep2( NULL ), m_playbackInfo( NULL ), m_volBar( NULL ), m_chanInfos(), m_chanCells(), m_trackerInfo( NULL ), m_modTitle( NULL ) {
