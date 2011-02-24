360a330093f2314b031a3333313668ce69e32932
Syron <mr.syron@googlemail.com>
2011-02-24 20:17:04 +0100

    made everything GPLv3+, some include guards renamed to make them consistent

--- src/output/mp3audiooutput.cpp
+++ src/output/mp3audiooutput.cpp
@@ -1,6 +1,6 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) 2011  Steffen Ohrendorf <email>
+    PeePeePlayer - an old-fashioned module player
+    Copyright (C) 2010  Syron <mr.syron@googlemail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 
 #include "mp3audiooutput.h"
 
