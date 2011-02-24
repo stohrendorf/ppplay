12c6fa7007e9ce325eb655c928dc4d59c8901ca9
Syron <mr.syron@googlemail.com>
2011-02-24 20:21:06 +0100

    another astyle deletion

--- src/xmmod/xminstrument.cpp
+++ src/xmmod/xminstrument.cpp
@@ -22,8 +22,26 @@
 
 #pragma pack(push,1)
 struct InstrumentHeader {
+	uint32_t size;
+	char name[22];
+	uint8_t type;
+	uint16_t numSamples;
 };
 struct InstrumentHeader2 {
+	uint32_t size;
+	uint8_t indices[96]; //!< @brief Maps note indices to their samples
+	struct { int16_t x,y; } volEnvelope[12];
+	struct { int16_t x,y; } panEnvelope[12];
+	uint8_t numVolPoints;
+	uint8_t numPanPoints;
+	uint8_t volSustainPoint;
+	uint8_t volLoopStart, volLoopEnd;
+	uint8_t panSustainPoint;
+	uint8_t panLoopStart, panLoopEnd;
+	uint8_t volType, panType;
+	uint8_t vibType, vibSweep, vibDepth, vibRate;
+	uint16_t volFadeout;
+	uint16_t reserved[11];
 };
 #pragma pack(pop)
 
