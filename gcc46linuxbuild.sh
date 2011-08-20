cc94e0939501fe8c0f498ec58dd1707ce10755ec
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2011-08-20 21:20:17 +0200

    updated the gcc46 build script to check for local installations

--- gcc46linuxbuild.sh
+++ gcc46linuxbuild.sh
@@ -1,6 +1,14 @@
 echo "CMake configuration script for GCC 4.6 usage on a linux host"
 echo "Copyright (C) 2011 by Steffen Ohrendorf <steffen.ohrendorf@gmx.de>"
 
+if ! test -e `which gcc-4.6`
+then
+	echo "Cannot find the 'gcc-4.6' executable in \$PATH."
+	echo "If your version of gcc is >= 4.6, use the preferred build procedure."
+	echo "gcc -dumpversion output:"
+	gcc -dumpversion
+fi
+
 if ! test -d "build"
 then
 	mkdir build
@@ -20,7 +28,7 @@ fi
 
 echo "Starting cmake configuration..."
 
-cmake -DCMAKE_CXX_COMPILER=/usr/bin/gcc-4.6 -DCMAKE_C_COMPILER=/usr/bin/gcc-4.6 -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+cmake -DCMAKE_CXX_COMPILER=`which gcc-4.6` -DCMAKE_C_COMPILER=`which gcc-4.6` -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 
 echo "Configured, now cd to 'build' and type 'make' to build PeePeePlayer."
 
