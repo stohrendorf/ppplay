b38b9176dfece025ecddb18da39130df2c3a0a25
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2011-12-09 06:01:57 +0100

    small pack-src.sh optimization

--- pack-src.sh
+++ pack-src.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-
-if [ "$1" == "" ]
-then
-	COMMIT=HEAD
-else
-	COMMIT=$1
-fi
-
+COMMIT=${1:-HEAD}
 VERSION=`git describe $COMMIT`
 git archive --prefix="peepeeplayer-$VERSION/" --format=tar -v $COMMIT | bzip2 --best -z > "peepeeplayer-$VERSION.tar.bz2"
 
