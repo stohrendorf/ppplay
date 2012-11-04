5795a5107271ccccc07cccf97e762075d352d765
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2012-11-04 05:03:02 +0100

    apply dosbox noise behaviour

--- src/ymf262/highhatoperator.cpp
+++ src/ymf262/highhatoperator.cpp
@@ -5,14 +5,10 @@ namespace opl
 {
 double HighHatOperator::getOperatorOutput( int modulator )
 {
-	double topCymbalOperatorPhase =
-		opl()->topCymbalOperator()->phase() * Operator::multTable[opl()->topCymbalOperator()->mult()];
-	// The sound output from the High Hat resembles the one from
-	// Top Cymbal, so we use the parent method and modifies his output
-	// accordingly afterwards.
-	double operatorOutput = TopCymbalOperator::getOperatorOutput( modulator, topCymbalOperatorPhase );
-	if( operatorOutput == 0 )
-		operatorOutput = rand() * std::pow(2, -envelope()/8.0) / RAND_MAX;
-	return operatorOutput;
+	uint16_t cymbalPhase = opl()->topCymbalOperator()->phase();
+	uint16_t phasebit = (((phase() & 0x88) ^ ((phase()<<5) & 0x80)) | ((cymbalPhase ^ (cymbalPhase<<2)) & 0x20)) ? 0x02 : 0x00;
+	uint16_t noisebit = rand()&1;
+	setPhase((phasebit<<8) | (0x34<<(phasebit ^ (noisebit<<1))));
+	return getOutput(modulator, phase(), ws());
 }
 }
--- src/ymf262/topcymbaloperator.cpp
+++ src/ymf262/topcymbaloperator.cpp
@@ -5,21 +5,14 @@ namespace opl
 {
 double TopCymbalOperator::getOperatorOutput( int modulator )
 {
-	int highHatOperatorPhase =
-		opl()->highHatOperator()->phase() * Operator::multTable[opl()->highHatOperator()->mult()];
 	// The Top Cymbal operator uses his own phase together with the High Hat phase.
-	return getOperatorOutput( modulator, highHatOperatorPhase );
-}
-double TopCymbalOperator::getOperatorOutput( int /*modulator*/, int externalPhase )
-{
+	uint16_t highHatPhase = opl()->highHatOperator()->phase();
+	uint16_t phaseBit = (((phase() & 0x88) ^ ((phase()<<5) & 0x80)) | ((highHatPhase ^ (highHatPhase<<2)) & 0x20)) ? 0x02 : 0x00;
+	
 	setEnvelope( envelopeGenerator()->getEnvelope( egt(), am() ) );
 
-	setPhase( phaseGenerator()->getPhase( vib() ) );
+	setPhase((1+phaseBit)<<8);
 
-	// Empirically tested multiplied phase for the Top Cymbal:
-	int carrierPhase = ( 8 * phase() ) & 0x3ff;
-	int modulatorPhase = externalPhase&0x3ff;
-	
 	uint8_t waveform = ws();
 	if( opl()->isNew() ) {
 		waveform &= 0x07;
@@ -28,12 +21,6 @@ double TopCymbalOperator::getOperatorOutput( int /*modulator*/, int externalPhas
 		waveform &= 0x03;
 	}
 	
-	double modulatorOutput = getOutput( Operator::noModulator, modulatorPhase, waveform );
-	double carrierOutput = getOutput( modulatorOutput*1024, carrierPhase, waveform );
-
-	int cycles = 4;
-	if( std::fmod( carrierPhase * cycles, cycles ) > 0.1 ) carrierOutput = 0;
-
-	return carrierOutput * 2;
+	return getOutput(0, phase(), waveform);
 }
 }
