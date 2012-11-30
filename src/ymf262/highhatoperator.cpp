3012b35c7fbfb2bb3d99b1e9f3a1006a7fe7735f
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2012-11-30 06:42:41 +0100

    add verified random number generator to the opl3 class

--- src/ymf262/highhatoperator.cpp
+++ src/ymf262/highhatoperator.cpp
@@ -8,7 +8,7 @@ int16_t HighHatOperator::nextSample( uint16_t modulator )
 	uint16_t cymbalPhase = opl()->topCymbalOperator()->phase();
 	uint16_t thisPhase = phase();
 	uint16_t phasebit = ( ( ( thisPhase & 0x88 ) ^( ( thisPhase << 5 ) & 0x80 ) ) | ( ( cymbalPhase ^( cymbalPhase << 2 ) ) & 0x20 ) ) ? 0x02 : 0x00;
-	uint16_t noisebit = rand() & 1;
+	uint16_t noisebit = opl()->randBit();
 	setPhase( (( phasebit << 8 ) | ( 0x34 << ( phasebit ^( noisebit << 1 ) ) )) );
 	envelopeGenerator()->advance( egt(), am() );
 	return getOutput( modulator + phase(), ws() );
