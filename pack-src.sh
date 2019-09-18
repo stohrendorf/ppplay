ac4afa17537628db67ab402d56b98e9f9f9a209a
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2019-09-18 08:31:33 +0200

    Delete pack-src.sh

--- pack-src.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-COMMIT=${1:-HEAD}
-VERSION=`git describe $COMMIT`
-git archive --prefix="ppplay-$VERSION/" --format=tar -v $COMMIT | bzip2 --best -z > "ppplay-$VERSION.tar.bz2"
-git archive --prefix="ppplay-$VERSION/" --format=tar.gz -v $COMMIT > "ppplay-$VERSION.tar.gz"
-git archive --prefix="ppplay-$VERSION/" --format=zip -v $COMMIT > "ppplay-$VERSION.zip"
